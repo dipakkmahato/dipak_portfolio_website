e4ffee6223891a0b86bed48a07f7957259ea98e8
Dipak Kumar Singh <106677227+dipakkmahato@users.noreply.github.com>
2026-09-24 22:43:05 +0545

    Change 'link' to 'cover' in site.ts
    Updated the property 'link' to 'cover' in site data.

--- src/data/site.ts
+++ src/data/site.ts
@@ -203,7 +203,7 @@ export const projects: Project[] = [
     type: "Website",
     tags: ["Reactjs", "Nodejs", "MongoDB", "Full-Stack"],
     services: ["Web Development"],
-    link: "https://i.postimg.cc/76RR74LL/yeslme.jpg",
+    cover: "https://i.postimg.cc/76RR74LL/yeslme.jpg",
     link: "https://doyouloveme143.netlify.app/",
   },
   {
